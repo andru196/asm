296fa747f97aa7cfc553682881dc63dbd850520b
andru196 <andru196@icloud.com>
2020-07-21 00:29:44 +0300

    testttttttttttttt

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
-rm -f champs/*/*.cor
+rm -rf corFiles/*/*.cor
+date="$(date)"
 for i in $(find champs -name *.s); 
  do 
 	
-	./asm $i;
+	./asm $i >> corFiles/our/run$date.log
 	f="$(basename -s .s $i)"
 	echo "$f"
 
 	chmod 777 champs/$f.cor 
-	#./school-asm $i
+	#./school-asm $i >> corFiles/school/run$date.log
 	#mv champs/*.cor corFiles/school
 	mv champs/*.cor corFiles/our
 	#diff corFiles/our/$f corFiles/school/$f >> result
