d43275b25b2a7b7ffb781ebcd473ff9ec4368c98
andru196 <andru196@icloud.com>
2020-12-11 13:58:30 +0300

    fix norm

--- src/corewar/src/print/print_dump_lobby.c
+++ src/corewar/src/print/print_dump_lobby.c
@@ -3,68 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   print_dump_lobby.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mschimme <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: andru <andru@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/23 22:30:43 by mschimme          #+#    #+#             */
-/*   Updated: 2020/12/11 01:26:08 by mschimme         ###   ########.fr       */
+/*   Updated: 2020/12/11 13:56:57 by andru            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cwr.h>
 
 /*
-TD	ДОПИСАТЬ вывод результата (и фсё);
-TD:	Декларация - заглушка. Подобрать список нужных параметров.
-!Контракт:
-	*	Просто выводит состояние арены в текущий момент.
-	*	Принимает указатель на указатель головы t_vasa, которые в своем 
-	* ->gen.cyc_sol хранят циклы, в которые необхоимо дампнуться.
-	*	Через голову управляет цепочкой дампов.
-	*	Отпработав последний дамп-цикл должна подать сигнал на прерывание работы
-	*	цикла (возвратить 1).
-
-! Арена начинается на &arena[sizeof(RTP)]. "Уши" по бокам - буферные, для работы
-	! машинными словами при чтении / вставке данных на арену. Эти уши должны
-	! быть за рамками функций чтения и записи на арена затерты '\0', потому
-	! в выводе полезной нагрузке не имеют.
-! do ... while запрещен по норме.
-
-
+**	Просто выводит состояние арены в текущий момент.
+**	Принимает указатель на указатель головы t_vasa, которые в своем
+** ->gen.cyc_sol хранят циклы, в которые необхоимо дампнуться.
+**	Через голову управляет цепочкой дампов.
+**	Отпработав последний дамп-цикл должна подать сигнал на прерывание работы
+**	цикла (возвратить 1).
 */
+
+void			set_dumpline(char str[64 * 2 + 64], uint8_t **bytes, size_t *i,
+																	size_t max)
+{
+	size_t	cpy;
+	uint8_t	*bytes_cpy;
+	uint8_t	tmp;
+
+	cpy = *i;
+	bytes_cpy = *bytes;
+	ft_bzero(str, sizeof(char) * (64 * 3));
+	while ((++cpy % 0x40) && cpy < max)
+	{
+		tmp = *bytes_cpy / 16;
+		*str++ = tmp + (tmp > 9 ? 'A' - 10 : '0');
+		tmp = *bytes_cpy++ % 16;
+		*str++ = tmp + (tmp > 9 ? 'A' - 10 : '0');
+		*str++ = ' ';
+	}
+	str[64 * 3 - 1] = 0;
+	*bytes = bytes_cpy;
+	*i = cpy;
+}
+
 uint8_t			ft_print_dump(t_world *nexus)
 {
-	t_vasa		*ptr;
-	t_vasa		**cycle;
-	char 		str[64 * 2 + 64];
-	char 		*str_cpy;
-	uint8_t		*bytes;
+	t_vasa			*ptr;
+	char			str[64 * 2 + 64];
+	uint8_t			*bytes;
+	size_t			i;
+	const size_t	max = MEM_SIZE;
 
-	cycle = &nexus->cyc.cyc_to_dump;
-	ptr = *cycle;
-
-	bytes = nexus->arena;
-	size_t i = 0;
-	size_t max = MEM_SIZE + sizeof(RTP) * 2;
+	i = 0;
+	ptr = nexus->cyc.cyc_to_dump;
+	bytes = nexus->arena + sizeof(RTP);
 	while (i < max)
 	{
-		str_cpy = str;
-		ft_printf("%.8x:\t", i);
-		do
-		{
-			uint8_t tmp = *bytes / 16;
-			*str_cpy++ = tmp +  (tmp > 9 ? 'A' - 10 : '0');
-			tmp = *bytes++ % 16;
-			*str_cpy++ = tmp + (tmp > 9 ? 'A' - 10 : '0');
-			*str_cpy++ = ' ';
-			i++;
-		} while ((i % 0x40) && i < max);
-		str[64 * 2 + 63] = 0;
+		ft_printf("%.5x:\t", i);
+		set_dumpline(str, &bytes, &i, max);
 		ft_putendl(str);
 	}
 	ft_putendl("");
-	*cycle = (*cycle)->next;
+	nexus->cyc.cyc_to_dump = nexus->cyc.cyc_to_dump->next;
 	ptr->gen.cyc_sol = 0;
 	free(ptr);
-	ptr = *cycle;
-	return (*cycle == NULL);
+	return (nexus->cyc.cyc_to_dump == NULL);
 }
