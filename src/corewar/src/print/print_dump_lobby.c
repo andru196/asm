f61dc9975a231bada7c6b5c8d4f413343b31bfdb
sfalia-f <sfalia-f@ga-r3.msk.21-school.ru>
2020-12-26 17:58:41 +0300

    dump fix

--- src/corewar/src/print/print_dump_lobby.c
+++ src/corewar/src/print/print_dump_lobby.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   print_dump_lobby.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: andru <andru@student.42.fr>                +#+  +:+       +#+        */
+/*   By: sfalia-f <sfalia-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/23 22:30:43 by mschimme          #+#    #+#             */
-/*   Updated: 2020/12/11 13:56:57 by andru            ###   ########.fr       */
+/*   Updated: 2020/12/26 17:56:58 by sfalia-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cwr.h>
+
+# define LINE_SIZE 64 * 3
 
 /*
 **	Просто выводит состояние арены в текущий момент.
@@ -21,39 +23,50 @@
 **	цикла (возвратить 1).
 */
 
-void			set_dumpline(char str[64 * 2 + 64], uint8_t **bytes, size_t *i,
+static inline void	set_dumpline(char str[LINE_SIZE], uint8_t **bytes, size_t *i,
 																	size_t max)
 {
 	size_t	cpy;
 	uint8_t	*bytes_cpy;
 	uint8_t	tmp;
+	char	*str_cpy;
 
 	cpy = *i;
+	str_cpy = str;
 	bytes_cpy = *bytes;
-	ft_bzero(str, sizeof(char) * (64 * 3));
-	while ((++cpy % 0x40) && cpy < max)
+	ft_bzero(str, sizeof(char) * (LINE_SIZE));
+	while (cpy < max)
 	{
 		tmp = *bytes_cpy / 16;
 		*str++ = tmp + (tmp > 9 ? 'A' - 10 : '0');
 		tmp = *bytes_cpy++ % 16;
 		*str++ = tmp + (tmp > 9 ? 'A' - 10 : '0');
 		*str++ = ' ';
+		if (!(++cpy % 0x40))
+			break ;
 	}
-	str[64 * 3 - 1] = 0;
+	str_cpy[LINE_SIZE - 1] = '\0';
 	*bytes = bytes_cpy;
 	*i = cpy;
 }
 
-uint8_t			ft_print_dump(t_world *nexus)
+uint8_t				ft_print_dump(t_world *nexus)
 {
 	t_vasa			*ptr;
-	char			str[64 * 2 + 64];
+	char			str[LINE_SIZE];
 	uint8_t			*bytes;
 	size_t			i;
 	const size_t	max = MEM_SIZE;
 
 	i = 0;
 	ptr = nexus->cyc.cyc_to_dump;
+	bytes = nexus->arena + sizeof(RTP) + max / 2;
+	while (i < (max / 2))
+	{
+		ft_printf("%.5x:\t", i);
+		set_dumpline(str, &bytes, &i, max);
+		ft_putendl(str);
+	}
 	bytes = nexus->arena + sizeof(RTP);
 	while (i < max)
 	{
