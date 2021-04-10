8e0bf280320ebe32c43595b09d98cf4522e393db
Ysgramor Corrupted shade <ycorrupt@am-k3.msk.21-school.ru>
2021-04-10 19:34:22 +0300

    fix merge error

--- src/corewar/src/op/op_13_lld.c
+++ src/corewar/src/op/op_13_lld.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   op_13_lld.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mschimme <mschimme@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ycorrupt <ycorrupt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/25 18:22:49 by mschimme          #+#    #+#             */
-/*   Updated: 2021/04/10 17:00:56 by mschimme         ###   ########.fr       */
+/*   Updated: 2021/04/10 19:25:17 by ycorrupt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ void		op_lld(t_world *nexus, t_carry *carry, \
 	}
 	carry->op = 0;
 	carry->pos += op_cont.length;
+	update_visual_carry(carry, op_cont.length, nexus);
 	carry->exec_cyc++;
 }
 
--- src/corewar/src/op/op_14_lldi.c
+++ src/corewar/src/op/op_14_lldi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   op_14_lldi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mschimme <mschimme@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ycorrupt <ycorrupt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/25 18:22:49 by mschimme          #+#    #+#             */
-/*   Updated: 2021/04/10 17:01:11 by mschimme         ###   ########.fr       */
+/*   Updated: 2021/04/10 19:25:32 by ycorrupt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ void			op_lldi(t_world *nexus, t_carry *carry, \
 	}
 	carry->op = 0;
 	carry->pos += op_cont.length;
+	update_visual_carry(carry, op_cont.length, nexus);
 	carry->exec_cyc++;
 }
 
